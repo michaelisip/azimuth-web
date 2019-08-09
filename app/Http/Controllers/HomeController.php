8be2306a5e64509ff12ab4d2246b6b7f659e4020
Michael John Isip <mj.isip123@gmail.com>
2019-08-10 06:48:10 +0800

    [FEATURE] Added Practice Mode

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Quiz;
+use App\Question;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,25 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('home', ['quizzes' => Quiz::with('questions')->get()]);
+    }
+
+    public function practiceMode($quiz)
+    {
+        return view('user.practice_mode', ['quiz' => Quiz::findOrFail($quiz)]);
+    }
+
+    public function check(Request $request)
+    {
+
+        $correct = Question::where([
+            'quiz_id' => $request->quiz_id,
+            'id' => $request->question_id])->value('answer');
+
+        if ($correct == $request->student_answer) {
+            return response()->json(['result' => 'correct']);
+        }
+
+        return response()->json(['result' => 'wrong']);
     }
 }
