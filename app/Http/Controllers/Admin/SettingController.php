883f2f7b99fb0a37594749bc2450354bd861ee21
Michael John Isip <mj.isip123@gmail.com>
2019-08-15 23:09:19 +0800

    modals

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class SettingController extends Controller
 {
     public function index()
     {
-        return view('admin.settings');
+        return view('admin.settings', ['user' => Auth::user()]);
     }
 }
